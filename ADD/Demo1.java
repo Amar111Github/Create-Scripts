2237e88b8980e4b7f4940c7fe4b23543c17480b5
Amar111Github <116936028+Amar111Github@users.noreply.github.com>
2022-10-29 11:48:43 +0530

    Add files via upload

---
+++ ADD/Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        long a=10;
+        long b=20;
+        long c=a+b;
+        System.out.println(c);
+    }
+    
+}
---
+++ ADD/Demo2.java
@@ -0,0 +1,12 @@
+class Demo2 {
+    public static void main(String[] args) {
+        try{
+        int a=20/0;
+        }
+    catch(ArithmeticException e){System.out.println(e);}  
+
+           System.out.println("Welcome...");  
+
+    }
+}
+
